4ebedf048890dff2d200af4a3def3692d6414803
frankchang <frankchang@hitrust.com.tw>
2025-05-09 23:25:12 +0800

    [新增] 新增專案架構

---
+++ SmtpMailApplication/src/test/java/com/example/smtp/mail/SmtpMailApplication/SmtpMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smtp.mail.SmtpMailApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmtpMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
